0b06a4a0673e417adb7a8fb664e29e94ffbdb2a0
Philip Lowe <philip.lowe94@gmail.com>
2021-04-07 14:48:16 -0400

    wrote spec for user model

---
+++ goalapp/db/migrate/20210407183524_fix_users.rb
@@ -0,0 +1,9 @@
+class FixUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_index :users, :username
+    remove_index :users, :session_token
+
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
---
+++ goalapp/spec/models/users_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    subject(:phil) {User.new(username: 'phil', password: 'password') }
+    let(:incomplete_user) {User.new(not_a_username: 'user')}
+
+    describe 'validations' do
+        it { should validate_presence_of(:username) }
+        it { should validate_presence_of(:password_digest) }
+        it { should validate_presence_of(:session_token) }
+        it { should validate_uniqueness_of(:username) }
+        it { should validate_uniqueness_of(:session_token) }
+        it { should validate_length_of(:password).is_at_least(6) }
+    end
+end
